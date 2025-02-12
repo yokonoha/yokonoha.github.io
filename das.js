46c2a3d47ed5c5d884b5d4801bf8acafbc4471cb
横茶横葉/Y.Yokoha <askjp.team@gmail.com>
2025-02-13 00:38:12 +0900

    Add files via upload

---
+++ das.js
@@ -0,0 +1,36 @@
+//////////////////////////////////////
+////    Dislike Archive Script    ////
+////     Prevent displaying       ////
+////           Beta!              ////
+//////////////////////////////////////
+
+// The author is not responsible for this script. Use at your own risk.
+//Please Copy&Paste when you want to implement this script to your works!
+//  <script src="./das.js"></script>
+
+//Variable
+let protectionlevel=1;//Default value is 1 Range 1~2 (2 is the highest.)
+
+if (protectionlevel=1)
+{
+if (navigator.userAgent.includes("archive"))
+{
+    console.log("[das] Suspicious UA detected. Trying to protect content by hiding...");
+    document.body.style.display="none";
+}
+
+}
+else
+{
+    if (navigator.userAgent.includes("archive"))
+        {
+            console.log("[das] Suspicious UA detected. Trying to protect content by hiding...");
+            document.body.style.display="none";
+            
+        }
+        else if(navigator.userAgent.includes("ia"))
+        {
+            console.log("[das lev2] Suspicious UA detected. Trying to protect content by hiding...");
+            document.body.style.display="none";
+        }
+}
