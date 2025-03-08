2392dc9446a3bff8d3b27e04dff15577eabdf89e
YokochaYokoha <askjp.team@gmail.com>
2025-03-09 02:03:16 +0900

    Re Change 20250309

--- acnt.js
+++ acnt.js
@@ -14,3 +14,22 @@
                alert("管理者パスワードを入力してください。");
             }
         }
+//20250309 に追加(補助スクリプト)
+document.getElementById("twbtn").addEventListener("click",()=>
+    {
+        window.open("https://x.com/yokocha_yokoha","_blank");
+    });
+    document.getElementById("biobtn").addEventListener("click",()=>
+    {
+        alert("Coming Soon!");
+       // window.open("https://yokonoha.github.io/biography","_blank");
+    });
+    document.getElementById("gtbtn").addEventListener("click",()=>
+    {
+        window.open("https://github.com/yokonoha/","_blank");
+    });
+    document.getElementById("wkbtn").addEventListener("click",()=>
+    {
+        alert("Coming Soon!");
+       // window.open("https://yokonoha.github.io/works","_blank");
+    });
