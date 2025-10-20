c81188b705ba6b5e2132931f64439f9068cf8163
横茶横葉/Y.Yokoha <askjp.team@gmail.com>
2025-10-20 11:28:17 +0900

    Update dcs.js

--- dcs.js
+++ dcs.js
@@ -3,4 +3,4 @@ const a=["https://","http://","ftp://","content://","file://"];
   const c="1234567890";
   const d=`!"#$%&'()=~-^@|*+<>,./?_`;
   const e="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const A1=b[18]+b[3]+b[24]+b[20]+b[10]+b[0]+b[17]+b[8];
+console.log("DC Loaded");
