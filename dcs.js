ddba72ff326125305b821737c8f3f683f77fa79e
横茶横葉/Y.Yokoha <askjp.team@gmail.com>
2025-10-20 11:26:46 +0900

    Add initial constants for URL schemes and characters

---
+++ dcs.js
@@ -0,0 +1,6 @@
+const a=["https://","http://","ftp://","content://","file://"];
+  const b="abcdefghijklmnopqrstuvwxyz";
+  const c="1234567890";
+  const d=`!"#$%&'()=~-^@|*+<>,./?_`;
+  const e="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const A1=b[18]+b[3]+b[24]+b[20]+b[10]+b[0]+b[17]+b[8];
