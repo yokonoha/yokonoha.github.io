03d10923b99dab5579122be30384f6ef2a99fa2b
YokochaYokoha <askjp.team@gmail.com>
2025-10-23 21:41:11 +0900

    Change CD & its design

--- dcs.js
+++ dcs.js
@@ -1,6 +1,6 @@
-const a=["https://","http://","ftp://","content://","file://"];
-  const b="abcdefghijklmnopqrstuvwxyz";
-  const c="1234567890";
-  const d=`!"#$%&'()=~-^@|*+<>,./?_`;
-  const e="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-console.log("DC Loaded");
+        const a = ["https://", "http://", "ftp://", "content://", "file://"];
+        const b = "nseiujhxgwfcobmvqpktzdryal";
+        const c = "2130648795";
+        const d = `$^>'_?~|/=,(<#!@)-+&*"%.`;
+        const e = "YHOTKMAIGBZQEFVXNSLCWDRPUJ";
+console.log("Loaded");
